5dd70cc067f0419bb9b073110f284c78b527f102
Dmutre <dmytro.lesko@gmail.com>
2023-09-10 20:44:57 +0200

    0.2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-
-function Main() {
-  const [num, setNum] = useState(15);
-
-  const increase = () => {
-    setNum(num+1);
-  }
-  
-  console.log(num);
-  return(
-    <div>
-    <Header num={num}/>
-    Hello
-    <button onClick={increase}>Hello button</button>
-  </div>
-  )
-}
-
-function Header({num}) {
-  return (
-    <header>Header {num}</header>
-  )
-}
+import App from './App.js';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
-  <Main/>
+  <App />
 );
